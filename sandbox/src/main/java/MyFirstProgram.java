dfeef2703ee11d9d520ff707ebdbda15759091d9
Daria Rekunova <d.rekunova@quality-lab.ru>
2019-05-21 21:13:43 +0700

    Создана функция вычисления расстояния между точками

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,17 @@
 public class MyFirstProgram {
 
- public static void main(String[] args) {
-System.out.println("Hello, world!");
+  public static void main(String[] args) {
+   Point p1 = new Point (1.0, 1.0);
+   Point p2 = new Point (1.0, 2.0);
+   System.out.println("Результат = " + distance(p1, p2));
  }
 
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(square(p1.x - p2.x) + square(p1.y - p2.y));
+  }
+
+  public static double square(double n) {
+    return n * n;
+  }
 }
+
