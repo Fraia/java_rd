fcabd107e0029fa624d2d9805e4734ea80504eb1
Daria Rekunova <d.rekunova@quality-lab.ru>
2019-05-31 17:14:17 +0700

    Заменены длинные локаторы в тесте Contact Creation Tests

--- addressbook-web-tests/src/test/java/ru/rd/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/rd/addressbook/ContactCreationTests.java
@@ -25,7 +25,7 @@ public class ContactCreationTests {
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+    wd.findElement(By.xpath("//input[@value='Login']")).click();;
   }
 
   @Test
@@ -36,7 +36,7 @@ public class ContactCreationTests {
   }
 
   private void saveContact() {
-    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
   }
 
   private void fillContactForm(ContactData contactData) {
