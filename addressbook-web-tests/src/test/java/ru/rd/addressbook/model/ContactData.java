418637a62d0975d5ec33150b4edf0c710ca62d24
Daria Rekunova <d.rekunova@quality-lab.ru>
2019-06-26 22:16:19 +0700

    Реализованы  генераторы тестовых данных для групп и для контактов, которые сохраняют данные в файл в формате XML

--- addressbook-web-tests/src/test/java/ru/rd/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/rd/addressbook/model/ContactData.java
@@ -1,8 +1,13 @@
 package ru.rd.addressbook.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.util.Objects;
+@XStreamAlias("contact")
 
 public class ContactData {
+    @XStreamOmitField
     private int id = Integer.MAX_VALUE;
     private String firstname;
     private String lastname;
