87d61209b211e119f40833d476bff99b88419dbe
Daria Rekunova <d.rekunova@quality-lab.ru>
2019-06-27 22:22:43 +0700

    Реализованы  провайдеры тестовых данных, которые читают данные из файлов в формате XML. Для тестов групп и контактов

--- addressbook-web-tests/src/test/java/ru/rd/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/rd/addressbook/tests/ContactCreationTests.java
@@ -1,14 +1,19 @@
 package ru.rd.addressbook.tests;
 
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.rd.addressbook.model.ContactData;
 import ru.rd.addressbook.model.Contacts;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,11 +21,28 @@ import static org.testng.Assert.assertEquals;
 
 
 public class ContactCreationTests extends TestBase {
-  @Test
-  public void testContactCreation(){
+
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>)xstream.fromXML(xml);
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+
+  }
+
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact){
     Contacts before=app.contact().all();
-    ContactData contact = new ContactData().withFirstname("Ivan")
-            .withLastname("Ivanov").withHomePhone("89046555467").withEmail("ivan@mail.ru").withGroup("test1");
     app.contact().create(contact);
     Contacts after=app.contact().all();
     assertThat(after.size(), equalTo(before.size() +1));
